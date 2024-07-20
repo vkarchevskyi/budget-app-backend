fb5a44fae57927a3858eceeb0b39838d11406ac7
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-07-20 21:01:56 +0300

    Fix: move logic from LoginRequest to services.

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,18 +6,41 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Services\Auth\Login\EnsureIsNotRateLimitedService;
+use App\Services\Auth\Login\GetThrottleKeyService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticatedSessionController extends Controller
 {
+    public function __construct(
+        private readonly EnsureIsNotRateLimitedService $ensureIsNotRateLimitedService,
+        private readonly GetThrottleKeyService $getThrottleKeyService
+    ) {
+    }
+
     /**
      * Handle an incoming authentication request.
+     * @throws ValidationException
      */
     public function store(LoginRequest $request): Response
     {
-        $request->authenticate();
+        $key = $this->getThrottleKeyService->run($request->input('email'), $request->ip());
+
+        $this->ensureIsNotRateLimitedService->run($request, $key);
+
+        if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+            RateLimiter::hit($key);
+
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($key);
 
         $request->session()->regenerate();
 
---
+++ app/Services/Auth/Login/GetThrottleKeyService.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Auth\Login;
+
+use Illuminate\Support\Str;
+
+readonly class GetThrottleKeyService
+{
+    public function run(string $email, string $ip): string
+    {
+        return Str::transliterate(Str::lower($email) . '|' . $ip);
+    }
+}
