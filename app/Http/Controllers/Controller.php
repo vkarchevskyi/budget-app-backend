e60fddb59f85c5e712ebfb95a5a683da35ddd151
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-09-08 12:58:55 +0300

    feat(swagger): update package, create Info block.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    version: '1.0.0',
+    description: 'Budget App API',
+    title: 'Budget App API',
+    contact: new OA\Contact(
+        name: 'vkarchevskyi',
+        email: 'vkarchevskyi10@gmail.com'
+    ),
+)]
 abstract class Controller
 {
     //
