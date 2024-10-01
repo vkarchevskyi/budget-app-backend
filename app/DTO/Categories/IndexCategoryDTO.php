3644e6540aa9dab212c75060b757c93aaa065beb
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-10-01 11:52:17 +0300

    feat(categories): create IndexCategoryDTO, update create and update DTO.

--- app/DTO/Categories/IndexCategoryDTO.php
+++ app/DTO/Categories/IndexCategoryDTO.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\DTO\Categories;
 
+use App\DTO\BasePaginationDTO;
 use Spatie\LaravelData\Attributes\MapName;
-use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+use Spatie\LaravelData\Optional;
 
 #[MapName(SnakeCaseMapper::class)]
-class CreateCategoryDTO extends Data
+class IndexCategoryDTO extends BasePaginationDTO
 {
-    public string $name;
+    public string|Optional $search;
     public int $userId;
 }
--- app/DTO/Categories/UpdateCategoryDTO.php
+++ app/DTO/Categories/UpdateCategoryDTO.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\DTO\Categories;
 
+use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+use Spatie\LaravelData\Optional;
 
+#[MapName(SnakeCaseMapper::class)]
 class UpdateCategoryDTO extends Data
 {
-    public function __construct(public string $name)
-    {
-    }
+    public string|Optional $name;
 }
