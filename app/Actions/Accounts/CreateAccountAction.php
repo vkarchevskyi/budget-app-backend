4f97636dd58ea42204f211fe6cffb566c329db8e
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-10-01 09:12:20 +0300

    feat(accounts): add action-level validation for account actions.

--- app/Actions/Accounts/CreateAccountAction.php
+++ app/Actions/Accounts/CreateAccountAction.php
@@ -7,11 +7,18 @@ namespace App\Actions\Accounts;
 use App\DTO\Accounts\CreateAccountDTO;
 use App\Models\Account;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 readonly class CreateAccountAction
 {
+    /**
+     * @throws ValidationException
+     */
     public function run(CreateAccountDTO $createAccountDTO): Account
     {
+        $this->validate($createAccountDTO);
+
         return DB::transaction(
             fn (): Account => Account::query()->create([
                 'name' => $createAccountDTO->name,
@@ -19,5 +26,16 @@ readonly class CreateAccountAction
                 'user_id' => $createAccountDTO->userId,
             ])
         );
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    private function validate(CreateAccountDTO $createAccountDTO): void
+    {
+        Validator::make($createAccountDTO->all(), [
+            'name' => 'required|string|max:255',
+            'user_id' => 'required|integer|min:1|exists:users,id',
+        ])->validate();
     }
 }
--- app/Actions/Accounts/UpdateAccountAction.php
+++ app/Actions/Accounts/UpdateAccountAction.php
@@ -7,11 +7,18 @@ namespace App\Actions\Accounts;
 use App\DTO\Accounts\UpdateAccountDTO;
 use App\Models\Account;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 readonly class UpdateAccountAction
 {
+    /**
+     * @throws ValidationException
+     */
     public function run(Account $account, UpdateAccountDTO $updateAccountDTO): Account
     {
+        $this->validate($updateAccountDTO);
+
         return DB::transaction(
             function () use ($account, $updateAccountDTO): Account {
                 $account->update($updateAccountDTO->all());
@@ -19,5 +26,15 @@ readonly class UpdateAccountAction
                 return $account;
             }
         );
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    private function validate(UpdateAccountDTO $updateAccountDTO): void
+    {
+        Validator::make($updateAccountDTO->all(), [
+            'name' => 'sometimes|required|string|max:255',
+        ])->validate();
     }
 }
