c734154f505f3481bb157d7447511dcacfc5fecd
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-10-01 18:54:14 +0300

    feat(transaction): add cast to TransactionResource date fields.

--- app/Resources/Transactions/TransactionResource.php
+++ app/Resources/Transactions/TransactionResource.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Resources\Transactions;
 
+use Carbon\CarbonInterface;
 use Illuminate\Support\Carbon;
 use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
@@ -20,9 +23,17 @@ class TransactionResource extends Data
         public readonly int $userId,
         public readonly int $accountId,
         public readonly int $price,
+
+        #[WithCast(DateTimeInterfaceCast::class, format: CarbonInterface::DEFAULT_TO_STRING_FORMAT)]
         public readonly Carbon $date,
+
+        #[WithCast(DateTimeInterfaceCast::class, format: CarbonInterface::DEFAULT_TO_STRING_FORMAT)]
         public readonly Carbon $createdAt,
+
+        #[WithCast(DateTimeInterfaceCast::class, format: CarbonInterface::DEFAULT_TO_STRING_FORMAT)]
         public readonly Carbon $updatedAt,
+
+        #[WithCast(DateTimeInterfaceCast::class, format: CarbonInterface::DEFAULT_TO_STRING_FORMAT)]
         public readonly ?Carbon $deletedAt
     ) {
     }
