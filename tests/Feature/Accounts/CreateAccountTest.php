e5b9ef6471b9709d0c1c2744b1b5d05d677384bf
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-10-06 13:48:49 +0300

    fix(tests): rewrite CreateAccountTest using fake HTTP requests.

---
+++ tests/Feature/Accounts/CreateAccountTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+test('can create a new account', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->postJson('/api/accounts', [
+        'name' => 'Cash',
+    ]);
+
+    $response
+        ->assertJson(fn (AssertableJson $json) => $json
+            ->where('id', 1)
+            ->where('name', 'Cash')
+            ->where('balance', 0)
+            ->hasAll(['created_at', 'updated_at'])
+            ->where('deleted_at', null)
+            ->etc()
+        );
+});
+
+test('can create a new account with a maximum name length', function () {
+    $user = User::factory()->create();
+    $name = str_repeat('a', 255);
+
+    $response = $this->actingAs($user)->postJson('/api/accounts', [
+        'name' => $name,
+    ]);
+
+    $response->assertJson(fn (AssertableJson $json) => $json
+        ->where('id', 1)
+        ->where('name', $name)
+        ->where('balance', 0)
+        ->hasAll(['created_at', 'updated_at'])
+        ->where('deleted_at', null)
+        ->etc()
+    );
+});
+
+test('cannot create a new account for non unauthenticated user', function () {
+    $response = $this->postJson('/api/accounts', [
+        'name' => 'Cash',
+    ]);
+
+    $response->assertUnauthorized();
+});
+
+test('cannot create a new account without a name', function () {
+    $user = User::factory()->create();
+    $response = $this->actingAs($user)->postJson('/api/accounts', []);
+
+    $response->assertInvalid([
+        'name' => 'The name field is required.',
+    ]);
+});
+
+test('cannot create a new account with very long name', function () {
+    $user = User::factory()->create();
+    $name = str_repeat('a', 255 + 1);
+
+    $response = $this->actingAs($user)->postJson('/api/accounts', [
+        'name' => $name,
+    ]);
+
+    $response->assertInvalid([
+        'name' => 'The name field must not be greater than 255 characters.',
+    ]);
+});
