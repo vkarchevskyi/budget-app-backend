a12883e17bc405f664d6260e94f6bee1ca52e16a
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-12-11 18:53:28 +0200

    feat(seeders): create DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Account;
+use App\Models\Category;
+use App\Models\Transaction;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,11 +19,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        $testUser = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        /** @var Collection<int, Account> $accounts */
+        $accounts = Account::factory(2)
+            ->state(['user_id' => $testUser->id])
+            ->sequence(['name' => 'Cash'], ['name' => 'Card'])
+            ->create();
+
+        /** @var Collection<int, Category> $categories */
+        $categories = Category::factory(5)
+            ->state(['user_id' => $testUser->id])
+            ->sequence(
+                ['name' => 'Bitcoin', 'is_income' => true],
+                ['name' => 'Cinema', 'is_income' => false],
+                ['name' => 'Groceries', 'is_income' => false],
+                ['name' => 'Books', 'is_income' => false],
+                ['name' => 'Salary', 'is_income' => true],
+            )
+            ->create();
+
+        Transaction::factory(1000)
+            ->state(
+                new Sequence(
+                    fn (Sequence $sequence): array => [
+                        'user_id' => $testUser->id,
+                        'account_id' => $accounts->random()->id,
+                        'category_id' => $categories->random()->id,
+                    ]
+                )
+            )
+            ->create();
     }
 }
