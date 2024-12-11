180fbf20bf3040b1fb36bc042bdf7f3792eb5a0a
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-12-11 20:18:52 +0200

    fix(routing): add name for every route

--- routes/api.php
+++ routes/api.php
@@ -16,38 +16,38 @@ Route::middleware(['auth:sanctum'])->group(function (): void {
     });
 
     Route::prefix('accounts')
-        ->name('accounts')
+        ->name('accounts.*')
         ->group(function (): void {
-            Route::get('', [AccountController::class, 'index']);
-            Route::post('', [AccountController::class, 'store']);
-            Route::get('{account}', [AccountController::class, 'show']);
-            Route::patch('{account}', [AccountController::class, 'update']);
-            Route::delete('{account}', [AccountController::class, 'destroy']);
+            Route::get('', [AccountController::class, 'index'])->name('index');
+            Route::post('', [AccountController::class, 'store'])->name('store');
+            Route::get('{account}', [AccountController::class, 'show'])->name('show');
+            Route::patch('{account}', [AccountController::class, 'update'])->name('update');
+            Route::delete('{account}', [AccountController::class, 'destroy'])->name('delete');
         });
 
     Route::prefix('categories')
-        ->name('categories')
+        ->name('categories.*')
         ->group(function (): void {
-            Route::get('', [CategoryController::class, 'index']);
-            Route::post('', [CategoryController::class, 'store']);
-            Route::get('{category}', [CategoryController::class, 'show']);
-            Route::patch('{category}', [CategoryController::class, 'update']);
-            Route::delete('{category}', [CategoryController::class, 'destroy']);
+            Route::get('', [CategoryController::class, 'index'])->name('index')->name('index');
+            Route::post('', [CategoryController::class, 'store'])->name('store');
+            Route::get('{category}', [CategoryController::class, 'show'])->name('show');
+            Route::patch('{category}', [CategoryController::class, 'update'])->name('update');
+            Route::delete('{category}', [CategoryController::class, 'destroy'])->name('delete');
         });
 
     Route::prefix('transactions')
-        ->name('transactions')
+        ->name('transactions.*')
         ->group(function (): void {
-            Route::get('', [TransactionController::class, 'index']);
-            Route::post('', [TransactionController::class, 'store']);
-            Route::get('{transaction}', [TransactionController::class, 'show']);
-            Route::patch('{transaction}', [TransactionController::class, 'update']);
-            Route::delete('{transaction}', [TransactionController::class, 'destroy']);
+            Route::get('', [TransactionController::class, 'index'])->name('index');
+            Route::post('', [TransactionController::class, 'store'])->name('store');
+            Route::get('{transaction}', [TransactionController::class, 'show'])->name('show');
+            Route::patch('{transaction}', [TransactionController::class, 'update'])->name('update');
+            Route::delete('{transaction}', [TransactionController::class, 'destroy'])->name('delete');
         });
 
     Route::prefix('charts')
-        ->name('charts')
+        ->name('charts.*')
         ->group(function (): void {
-            Route::get('expenses', [ChartController::class, 'expenses']);
+            Route::get('expenses', [ChartController::class, 'expenses'])->name('expenses');
         });
 });
